6148ad4b6ce9ed93c42c2454825df4dce06010f3
Sagar <sagarwaghmare1384@gmail.com>
2025-05-18 15:46:53 +0530

    Added index and setup of the mongodb

---
+++ server/src/db.ts
@@ -0,0 +1,15 @@
+// Here we write the database connection code
+
+import mongoose from 'mongoose';
+
+const connectDB=async():Promise<void> =>{
+    try{
+        const conn=await mongoose.connect(process.env.MONGODB_URI as string);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    }
+    catch(error){
+        console.log(`Error:${error as Error}.message`);
+        process.exit(1);
+    }
+}
+export default connectDB;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,33 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+// d.ts for the declation file
+const app = express();
+
+// Routes
+
+app.post('/api/v1/signup', (req, res) => {
+//   Handle user signup
+const {username,password}=req.body;
+//   const user = new User({ username, password });
+
+})
+
+app.post('/api/v1/signin', (req, res) => {
+
+})
+
+app.post('api/v1/content', (req, res) => {
+
+})
+
+
+app.post('/api/v1/verify', (req, res) => {
+
+})
+app.
+
+app.post('/api/v1/brain/:shareLink',(req,res)=>{
+
+})
+
