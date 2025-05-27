7f71a8cdadcca27815798ca28a1b9dfd874aecd1
Sagar <sagarwaghmare1384@gmail.com>
2025-05-28 00:20:21 +0530

    Added tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
-
-
-const App = () => {
+export default function App() {
   return (
-    <h1>Hello</h1>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
